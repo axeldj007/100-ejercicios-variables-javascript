796478f65314fb6ae5a9a9b64206415683f4b67e
axeldj007 <axelku9@gmail.com>
2025-06-27 12:36:38 -0600

    Add files via upload

---
+++ 100_ejercicios_variables_en_javascript.js
@@ -0,0 +1,439 @@
+let $nombre = "axel"
+console.log($nombre);
+
+let edad = 20;
+console.log(edad);
+
+let ciudad = "minatitlan veracruz";
+console.log(ciudad);
+
+let var1 = 1, var2 = 2, var3 = 3;
+console.log(var1,var2,var3);
+
+let altura = 10.55;
+console.log(altura);
+
+let activo = true;
+console.log(activo);
+
+let mensaje = null;
+console.log(mensaje);
+
+ let pais = "Mexico";
+ console.log(pais);
+
+ let precio = 100;
+ console.log(precio);
+
+ let cantidad = 5;
+ console.log(cantidad);
+
+ let total = precio*cantidad;
+ console.log(total);
+
+ let nombreCompleto = "Cristiano Ronaldo"
+ console.log(nombreCompleto);
+
+ let pi = 3.1416;
+ console.log(pi);
+
+ let IVA = 0.16;
+ console.log(IVA);
+
+ let esMayorEdad = false;
+ console.log(esMayorEdad);
+
+ let SaldoInicial = 500;
+ console.log(SaldoInicial);
+
+ let saldoFinal = SaldoInicial - 100;
+ console.log(saldoFinal);
+
+ let usuario = "Invitado.";
+ console.log(usuario);
+
+ let respuesta = null;
+ console.log(respuesta);
+
+ let esActivo;
+ console.log(esActivo);
+
+ let temperatura = -5;
+ console.log(temperatura);
+
+ let mes ='junio';
+ console.log(mes);
+
+ let ano = 2025;
+ console.log(ano);
+
+ let nacimiento = ano-edad;
+ console.log(nacimiento);
+
+ let esEstudiante = true;
+ console.log(esEstudiante);
+ 
+ let direccion = `colonia diaz ordaz, calle benito juarez, numero 5`;
+ console.log(direccion);
+
+ let ancho = 15;
+ let largo = 30;
+ console.log(ancho*largo);
+
+ let multiplicacion = 5*4;
+ console.log(multiplicacion);
+
+ let base = 20;
+ let alto = 50;
+ console.log(base*alto/2);
+
+ let radio = 5;
+ console.log( "el area del circulo es "+ Math.PI* radio*radio);
+
+ let nombre = "alexis";
+ let apellido = "sanchez";
+ console.log(nombre +" "+ apellido);
+
+ let horasTrabajadas = 8;
+ let pagoPorHora = 50;
+ console.log("El pago total es " +horasTrabajadas*pagoPorHora + " pesos");
+
+ let calificacion1 = 80;
+ let calificacion2 = 60;
+ let calificacion3 = 90;
+ let promedio = calificacion1+calificacion2+calificacion3
+ console.log("El promedio es "+ promedio/3);
+
+ let elNumero = 5;
+ let esPar = elNumero % 2 == 0;
+ console.log(esPar)
+ console.log("¿es par? " + esPar);
+
+ let metros = 100;
+ let centimetros = metros*1000;
+ console.log(metros + " metros son " + centimetros + " centimetros.");
+
+ let segundos = 60;
+ let minutos = segundos/60;
+ console.log(segundos + " segundos es " + minutos + " minutos");
+
+ let precioProducto = 100;
+ let descuento = (precioProducto/100)*20;
+ console.log(precioProducto-descuento);
+
+ let nombreDePersona = "Cristiano Ronaldo";
+ let numeroDeLetras = nombreDePersona.length-1;
+ console.log("La cantidad de letras del nombre es " + numeroDeLetras);
+
+ let nombre1 = "alex";
+ let apellido1 = " rodriguez";
+ let nombreCompleto1 = nombre1 + apellido1;
+ console.log("Mi nombre completo es " + nombreCompleto1);
+
+ let a = 5;
+ let b = 10;
+ let suma = a+b;
+ console.log(suma);
+
+ let base1 = 70;
+ let altura1 = 100;
+ let area = (base1*altura1)/2;
+ console.log(area);
+
+ let radio1 = 5;
+ let doblEDelRadio = radio1*2;
+ console.log(doblEDelRadio);
+
+ let ancho1 = 10
+ let alto1 = 20
+ let perimetroRectangulo = (ancho1+alto1)*2;
+ console.log("El perimetro del rectangulo es " + perimetroRectangulo);
+
+ let minutos1 = 60;
+ let segundos1 = minutos1*60;
+ console.log(minutos1 + " minutos son " + segundos1 +" segundos");
+
+ let metros1 = 150;
+ let kilometros1 = metros1/1000;
+ console.log("los kilometros son "+ kilometros1);
+
+ let dia = "25/";
+ let mes1 = "12/";
+ let ano1 = "2026";
+ let fecha = dia+mes1+ano1;
+ console.log(fecha);
+
+ let producto1 = 80;
+ let producto2 = 100;
+ let producto3 = 150;
+ let total1 = producto1+producto2+producto3;
+ console.log(total1);
+
+ let valor1 = 80;
+ let valor2 = 50;
+ let resta = valor1-valor2;
+ console.log(resta);
+
+ let cantidad1 = 5;
+ let precioUnitario = 10;
+ let totalAPagar = cantidad1*precioUnitario;
+ console.log(totalAPagar);
+
+ let nota11 = 100;
+ let nota22= 80;
+ let nota33 = 70;
+ let promedio11 = (nota11+nota22+nota33)/3;
+ console.log(promedio11);
+
+ let peso = 75;
+ let estatura = 168;
+ let IMC = peso/(estatura*estatura);
+ console.log(IMC*100);
+
+ let sueldoBase = 8000;
+ let bono = 1000;
+ let sueldoTotal = sueldoBase+bono;
+ console.log(sueldoTotal);
+
+ let distanciaEnMetros = 100;
+ let tiempoEnSegundos = 8;
+ let velocidadEnMs= distanciaEnMetros/tiempoEnSegundos;
+ console.log(velocidadEnMs);
+
+ let edad1 = 18
+ let edad2 = edad1>=18;
+ console.log(edad2);
+
+ let anioActual = 2025;
+ let anioNacimiento = 2003;
+ let edadCalculada = anioActual-anioNacimiento;
+ console.log(edadCalculada);
+
+ let x = 20;
+ let y = 30;
+ let z = 40;
+ let sumatotal = x+y+z;
+ console.log(sumatotal);
+
+ let ladoDelCuadrado = 5;
+ let area2 = ladoDelCuadrado*ladoDelCuadrado;
+ console.log(area2);
+
+ let horas = 5*3600;
+ let minutos2 = 35*60;
+ let segundos3 = 250;
+ let totalsegundos = horas+minutos2+segundos3;
+ console.log(totalsegundos);
+
+ let cantidadLibros = 4;
+ let Dias = 12;
+ let librosPorDia = cantidadLibros/Dias;
+ console.log(librosPorDia);
+
+ let producto5 = 'tomates';
+ let precio1 = 2;
+ let cantidad11 = 5;
+ let totalCompra = precio1*cantidad11;
+ console.log(totalCompra);
+
+ let a1 = 5;
+ let b1 = 6;
+ let esmayor = a1>b1;
+ console.log(esmayor);
+
+ let montoInicial = 1535;
+ let gasto = 650;
+ console.log(montoInicial-gasto);
+
+ let precio2 = 850;
+ let IVA1 = (precio2*16)/100;
+ console.log(precio2+IVA1);
+
+ let anios = 5;
+ let meses = 36;
+ console.log((anios*12)+meses);
+
+let metros11 = 150;
+let centimetros1 = 100;
+console.log((metros11*100)+centimetros1+ " cm");
+
+let number1 = 5;
+let number2 = 6;
+let number3 = 8;
+let prom = ((number1+number2+number3)/3);
+console.log(prom);
+
+let monto = 1557;
+let desc = (monto*20)/100;
+console.log(monto-desc);
+
+let alumnos = 1260;
+let salones = 15;
+console.log("los alumnos por salon son " + alumnos/salones);
+
+let viajes = 30;
+let litros = 15590;
+console.log('los litros por viaje son ' + litros/viajes);
+
+ let edadA= 25;
+ let edadVoto = edad >= 18;
+ console.log('puede votar ' + edadVoto);
+
+ let name1 = 'rodrigo';
+ let name2 = 'pepe';
+ let comprobar = name1 == name2;
+ console.log('¿es el mismo nombre? ' + comprobar);
+
+ let val1 = 377809;
+ let val2 = 664733;
+ let comprobar2 = val1 == val2;
+ console.log(comprobar2);
+
+ let monto1 = 455;
+ let monto2 = 655;
+ let monto3 = 443;
+ let comparacion1 = monto1>monto2>monto3;
+ console.log(comparacion1);
+ let comparacion2 = monto2>monto3;
+ console.log(comparacion2);
+ let resultado = 'el numero mayor es ' + monto2;
+ console.log(resultado);
+
+ let dias = 45;
+ let horas1 = dias*24;
+ console.log(horas1);
+
+ let puntos1 = 34;
+ let puntos2 = 45;
+ let puntos3 = puntos1>puntos2;
+ console.log(puntos3);
+ let ganador = 'el ganador es '+ puntos2;
+ console.log(ganador);
+
+ let edadUno = 34;
+ let edadDos = 35;
+ let comprobarTres = edadUno>35;
+ console.log(comprobarTres);
+ let comprobar3 = 'la edad '+ edadDos +' es mayor';
+ console.log(comprobar3);
+
+ let mesas = 35;
+ let Invitados = 250;
+ let invitadosPorMesa = Invitados/mesas;
+ console.log(invitadosPorMesa);
+
+ let horasTrabajadas1 = 40;
+ let pagoPorHora2 = 50;
+ let pagototal = horasTrabajadas1*pagoPorHora2;
+ console.log(pagototal);
+
+ let variable1 = 4;
+ let variable2 = variable1+4;
+ console.log(variable2);
+
+ let temperaturaC = 45;
+ let temperaturaF = (temperaturaC*9)/5+32;
+ console.log(temperaturaF);
+
+ let hijos = 4;
+ let padres = 2;
+ let personas = hijos+padres;
+ console.log(personas);
+
+ let numero = 233;
+ let doble = numero*2;
+ let triple = numero*3;
+ console.log(doble);
+ console.log(triple);
+
+ let cuotaMensual = 500;
+ let meses1 = 12;
+ let totalPagar = cuotaMensual*meses1;
+ console.log(totalPagar);
+
+ let alturaPersona = 1.75;
+ let alturaMedia = 1.80;
+ let esAlto = alturaPersona>alturaMedia;
+ console.log(esAlto);
+
+ let montototal = 355;
+ let montoPagado = 183;
+ let restante = montototal-montoPagado;
+ console.log(restante);
+
+ let anioNacimiento2 = 1993;
+ let anioActual1 = 2025;
+ let _edad = anioActual1-anioNacimiento2;
+ console.log(_edad);
+
+ let totalManzanas = 12;
+ let manzanasVendidas = 7;
+ let restan = totalManzanas-manzanasVendidas;
+ console.log(restan);
+
+ let asientosTotales = 190;
+ let asientosOcupados = 57;
+ let asientosDisponibles = asientosTotales-asientosOcupados;
+ console.log(asientosDisponibles);
+
+ let libros = 45;
+ let revistas = 12;
+ let totalLectura = libros+revistas;
+ console.log(totalLectura);
+
+ let velocidadKh = 155;
+ let velocidadMs = velocidadKh/3.6;
+ console.log(velocidadMs);
+
+ let gradosCelsius = 45;
+ let gradosKelvin = gradosCelsius + 273.15;
+ console.log(gradosKelvin);
+
+ let diasTrabajados = 28;
+ let sueldoDiario = 250;
+ let sueldoTotal1 = diasTrabajados*sueldoDiario;
+ console.log(sueldoTotal1);
+
+let alturaCm = 256;
+let alturaMts = alturaCm/100;
+console.log(alturaMts);
+
+let ahorroIncial = 10468;
+let deposito = 114832;
+let nuevoAhorro = ahorroIncial+deposito;
+console.log(nuevoAhorro);
+
+let nombreMascota = "Bola de nieve";
+let tipoDeAnimal = "felino";
+let frase = nombreMascota+ ' es '+tipoDeAnimal;
+console.log(frase);
+
+let cantidadHuevos = 155;
+let cantitdadDocena = cantidadHuevos/12;
+console.log(cantitdadDocena);
+
+let kilometros2 = 20;
+let litrosCombustible = 18;
+let rendimiento = kilometros2/litrosCombustible;
+console.log(rendimiento);
+
+let diasViaje = 15;
+let gastoDia = 230;
+let totalGastoViaje = diasViaje*gastoDia;
+console.log(totalGastoViaje);
+
+let panes = 30;
+let personas9 = 8;
+let panesPersona = panes/personas9;
+console.log(panesPersona);
+
+let mensajeFinal = 'ejercicios completados ';
+let k = 'exito';
+console.log(mensajeFinal+k);
+
+
+
+ 
+
+
+
